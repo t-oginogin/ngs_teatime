7074c5dafc9079442cee2ac93400e5d5a2519348
Tatsuya Ogi <t.oginogin@gmail.com>
2014-10-04 16:59:02 +0900

    select tools #2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def tool_labels
+    [%w[vicuna vicuna],
+     %w[sam_tools sam_tools],
+     %w[bwa bwa],
+     %w[bwa2 bwa2],
+     %w[bowtie bowtie],
+     %w[bowtie2 bowtie2]]
+  end
 end
