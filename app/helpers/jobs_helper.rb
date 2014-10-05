f9c08401440d90ce93301200be97bf46f540e0f1
Tatsuya Ogi <t.oginogin@gmail.com>
2014-10-05 10:16:31 +0900

    mapping Job status and abailable action #3

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,17 @@
 module JobsHelper
+  def schedulable( job )
+    job.status == 'created' || job.status == 'canceled'
+  end
+
+  def cancelable( job )
+    job.status == 'scheduled' || job.status == 'doing'
+  end
+
+  def editable( job )
+    job.status == 'created' || job.status == 'canceled' || job.status == 'error'
+  end
+
+  def deletable( job )
+    !(job.status == 'scheduled' || job.status == 'doing')
+  end
 end
